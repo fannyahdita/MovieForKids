f84012cfb6875738192691b6d711899475baa3fb
Fannyah Dita Cahya <fannyah.dita@ui.ac.id>
2019-12-11 14:35:05 +0700

    jni all done

--- jni/movieforkids.c
+++ jni/movieforkids.c
@@ -1,7 +1,7 @@
 #include "jni.h"
 #include "stdlib.h"
 
-JNIEXPORT jint JNICALL Java_id_ac_ui_cs_mobileprogramming_fannyah_dita_movieforkids_view_fragment_MovieListFragment_randommov
+JNIEXPORT jint JNICALL Java_id_ac_ui_cs_mobileprogramming_fannyah_1dita_movieforkids_view_fragment_MovieListFragment_randommov
   (JNIEnv * env, jobject obj) {
     int n;
     n = rand()%6;
