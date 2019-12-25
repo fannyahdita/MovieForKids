9e235caf55541cea6ca833c540ca7b91246fb280
Fannyah Dita Cahya <fannyah.dita@ui.ac.id>
2019-12-25 22:39:15 +0700

    fix background service. disables button for play music

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/fannyah_dita/movieforkids/view/activity/MusicActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/fannyah_dita/movieforkids/view/activity/MusicActivity.kt
@@ -1,7 +1,10 @@
 package id.ac.ui.cs.mobileprogramming.fannyah_dita.movieforkids.view.activity
 
+import android.app.ActivityManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import id.ac.ui.cs.mobileprogramming.fannyah_dita.movieforkids.R
 import id.ac.ui.cs.mobileprogramming.fannyah_dita.movieforkids.helper.MusicService
@@ -16,10 +19,29 @@ class MusicActivity : AppCompatActivity() {
 
         buttonStart.setOnClickListener {
             startService(Intent(this, MusicService::class.java))
+            buttonStart.isEnabled = false
+        }
+
+        if(isMyServiceRunning(MusicService::class.java)) {
+            Log.d("serviceMusic",(isMyServiceRunning((MusicService::class.java)).toString()))
+            buttonStart.isEnabled = false
+        } else {
+            Log.d("serviceMusic",(isMyServiceRunning((MusicService::class.java)).toString()))
         }
 
         buttonStop.setOnClickListener {
             stopService(Intent(this, MusicService::class.java))
+            buttonStart.isEnabled = true
         }
+    }
+
+    private fun isMyServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                return true
+            }
+        }
+        return false
     }
 }
